7a01e8d44ce8ec17c3a6d0655288922ac3dbf023
Tania Madrigal <129007972+TaniaMJ10@users.noreply.github.com>
2023-07-27 22:08:36 +0000

    footer

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+//include images into your bundle
+
+//create your first component
+const Footer = () => {
+    return (
+        <>
+            <div style={{backgroundColor: "black"}} className= "text-center p-4">
+               
+                    
+                        <h6  className="text-center"style={{color: "white"}} > 
+                        Copyright © Your Website 2023
+
+                        </h6>
+                 
+            
+            </div>
+        </>
+    );
+};
+
+export default Footer;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,14 +4,27 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import Navbar from "./Navbar.jsx";
 import Jumbotron from "./Jumbotron";
-
+import Card from "./Card";
+import Footer from "./Footer";
 //create your first component
 const Home = () => {
 	return (
 		<>
-		<Navbar/>
-		<Jumbotron/>
-		</>	
+			<Navbar />
+			<Jumbotron />
+			<div className="row">
+				<div className="col-3"><Card />
+				</div>
+				<div className="col-3"><Card />
+				</div>
+				<div className="col-3"><Card />
+				</div>
+				<div className="col-3"><Card />
+				</div>
+			</div>
+
+			<Footer />
+		</>
 	);
 };
 
